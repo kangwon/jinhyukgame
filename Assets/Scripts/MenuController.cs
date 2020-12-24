e4e0ca2ce3c377c2104e14019c1af0e3764e7ce8
kang_won@naver.com <753951zz>
2020-12-24 23:32:45 +0900

    Implement scene transition

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClickGameStartButton()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void OnClickRecordButton()
+    {
+        SceneManager.LoadScene("RecordScene");
+    }
+
+    public void OnClickOptionButton()
+    {
+        SceneManager.LoadScene("OptionScene");
+    }
+
+    public void OnClickCreditButton()
+    {
+        SceneManager.LoadScene("CreditScene");
+    }
+}
