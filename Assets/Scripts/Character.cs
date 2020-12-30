41d8dbad5a1c1db8871a586c56ef54c5a18ce408
quoobit <quoobt@gmail.com>
2020-12-30 20:56:51 +0900

    Character.cs에 기본적인 공격/방어 골자 추가, 이를 CombatController에서 관리하기위해 Script 추가 생성.

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -6,10 +6,10 @@ using System.Linq;
 [System.Serializable]
 public class Stat
 {
-    public int maxHp;
-    public int attack;
-    public int defense;
-    public int speed;
+    public int maxHp; //최대체력
+    public int attack; //공격력
+    public int defense; //방어력
+    public int speed; //속도
     public int startSpeedGauge;
 
     public static Stat operator +(Stat a, Stat b)
@@ -33,12 +33,14 @@ public class Stat
 public class CharacterBase
 {
     public readonly Stat baseStat;
-    public int hp;
+    public float hp;
+    public bool isMyTurn;
     public bool isDead
     {
         get => hp <= 0;
     }
 
+    
     public CharacterBase(Stat stat)
     {
         this.baseStat = stat;
@@ -49,11 +51,33 @@ public class CharacterBase
     {
         return baseStat;
     }
+
+    public virtual float AttackFoe() {return 0;} // 공격하는 함수
+
+    public virtual void TakeHit(float rawDamage) {} // 데미지 받는 함수
+    
 }
 
 public class Monster : CharacterBase 
 { 
     public Monster(Stat stat) : base(stat) { }
+    public string MonsterName; //몬스터 이름
+    public bool isBoss; //보스인지 아닌지
+
+    public override void TakeHit(float rawDamage) {
+        float afterDamage = rawDamage; // TODO : 추가 몬스터 기믹 추후 추가 
+        if(this.baseStat.defense >= rawDamage) {
+            hp = hp - 1;
+        } else {
+            hp = hp + this.baseStat.defense - rawDamage;
+        }
+    }
+
+    public override float AttackFoe() {
+        float finalDamage = this.baseStat.attack; //TODO : 공격 기믹 추가
+        return finalDamage;
+    }
+
 }
 
 public class Player : CharacterBase
@@ -74,5 +98,19 @@ public class Player : CharacterBase
         currentstat = buffs.Aggregate(currentstat, (stat, buff) => stat + buff);
         currentstat = items.Aggregate(currentstat, (stat, buff) => stat + buff);
         return currentstat;
+    }
+
+    public override void TakeHit(float rawDamage) {
+        float afterDamage = rawDamage; // TODO : 유물 등의 추가 방어 기믹 추후 추가 
+        if(this.baseStat.defense >= afterDamage) {
+            hp = hp - 1;
+        } else {
+            hp = hp + this.baseStat.defense - afterDamage;
+        }
+    }
+
+    public override float AttackFoe() {
+        float finalDamage = this.baseStat.attack;  //TODO : 공격 기믹 추가
+        return finalDamage;
     }
 }
