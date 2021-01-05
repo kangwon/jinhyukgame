b821556c97a1c9357eaf4c202ed571751e235b06
lee sangyeon <mpdark1227@gmail.com>
2021-01-06 00:45:09 +0900

    현재체력ui에 연동

--- Assets/Scripts/PlayerStatPanelUIController.cs
+++ Assets/Scripts/PlayerStatPanelUIController.cs
@@ -8,7 +8,7 @@ public class PlayerStatPanelUIController : MonoBehaviour
 {
     public Text ATK, DEF, SPD, HP;
     public Slider HP_slider;
-
+    Player player = GameState.Instance.player;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +16,6 @@ public class PlayerStatPanelUIController : MonoBehaviour
         DEF = GameObject.Find("DEF text").GetComponent<Text>();
         SPD = GameObject.Find("SPD text").GetComponent<Text>();
         HP = GameObject.Find("HP Txt").GetComponent<Text>();
-
         HP_slider = GameObject.Find("HP_Slider").GetComponent<Slider>();
         UpdateStat();
     }
@@ -29,12 +28,12 @@ public class PlayerStatPanelUIController : MonoBehaviour
 
     void UpdateStat()
     {
-        ATK.text = GameState.Instance.player.GetStat().attack.ToString();
-        DEF.text = GameState.Instance.player.GetStat().defense.ToString();
-        SPD.text = GameState.Instance.player.GetStat().speed.ToString();
+        ATK.text = player.GetStat().attack.ToString();
+        DEF.text = player.GetStat().defense.ToString();
+        SPD.text = player.GetStat().speed.ToString();
 
-        string hp_now = "17", hp_full; //임시로 설정
-        hp_full = GameState.Instance.player.GetStat().maxHp.ToString();
+        string hp_now = player.hp.ToString(), hp_full; //임시로 설정
+        hp_full = player.GetStat().maxHp.ToString();
         HP.text = "HP  " + hp_now + " / " + hp_full;
 
         HP_slider.value = Convert.ToInt32(hp_now);
