1e63cf570ace8f9329cba1ff1f44206566ee61df
sunder97 <sunder97@naver.com>
2021-01-06 20:39:00 +0900

    created Weapon Popup Screen

---
+++ Assets/Scripts/WeaponPopupView.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class WeaponPopupView : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+
+    public GameObject WeaponPopupScreen;
+    int popupCheck = 0;
+
+    public void OnPointerDown(PointerEventData data)
+    {
+        if (popupCheck == 0)
+        {
+            WeaponPopupScreen.SetActive(true);
+            popupCheck = 1;
+        }
+        else
+        {
+            WeaponPopupScreen.SetActive(false);
+            popupCheck = 0;
+        }
+    }
+
+    public void OnPointerUp(PointerEventData data)
+    {
+        // WeaponPopupScreen.SetActive(false);
+    }
+}
