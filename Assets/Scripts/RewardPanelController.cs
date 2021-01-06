8c0ed08106e6f6dc08abc4b0663a756fd892e078
gunddol1004 <gunddol1004@hanyang.ac.kr>
2021-01-06 12:39:58 +0900

    RewardPanel 생성

---
+++ Assets/Scripts/RewardPanelController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class RewardPanelController : MonoBehaviour
+{
+    public StageChoice stageChoice;
+    public void OnClickRewardAbandon()
+    {
+        stageChoice.MoveToNextStage();
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
