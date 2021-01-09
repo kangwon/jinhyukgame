c077ea679c1285aa1c223071f52c798485929f98
lee sangyeon <mpdark1227@gmail.com>
2021-01-10 03:40:07 +0900

    좌측하단 팝업창에 현재무기가 뜨게 설정

--- Assets/Scripts/WeaponPopupView.cs
+++ Assets/Scripts/WeaponPopupView.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using System.Linq;
 
 public class WeaponPopupView : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
@@ -17,7 +18,7 @@ public class WeaponPopupView : MonoBehaviour, IPointerDownHandler, IPointerUpHan
     {
         for(int i=0;i<10;i++)
         {
-            WeaponIcon[i] = GameObject.Find($"WeaponPopupView").transform.Find($"WeaponPopupView/WeaponPopupScreen/Weapon{i+1}");
+            WeaponIcon[i] = GameObject.Find($"WeaponPopupView").transform.Find($"WeaponPopupScreen/Weapon{i+1}").gameObject;
         }
     }
 
@@ -25,9 +26,31 @@ public class WeaponPopupView : MonoBehaviour, IPointerDownHandler, IPointerUpHan
     {
         if (popupCheck == 0)
         {
+            string temp;
             for(int i=0;i<10;i++)
-            {             
-                weaponIcon[i].transform.GetChild(0).GetComponent<Text>().text = $"{player.GetWeaponList().ElementAt(i).attack}";
+            {              
+                switch (player.GetWeaponList().ElementAt(i).weaponType)
+                {
+                    case WeaponType.sword:
+                        temp = "검";
+                        break;
+                    case WeaponType.blunt:
+                        temp = "둔기";
+                        break;
+                    case WeaponType.spear:
+                        temp = "창";
+                        break;
+                    case WeaponType.dagger:
+                        temp = "단검";
+                        break;
+                    case WeaponType.wand:
+                        temp = "지팡이";
+                        break;
+                    default:
+                        temp = "?";
+                        break;
+                }
+                WeaponIcon[i].transform.GetChild(0).GetComponent<Text>().text = $"{temp}";
             }
             WeaponPopupScreen.SetActive(true);
             popupCheck = 1;
