041a59913ee257563df2243ad34a88d09d94292f
quoobit <quoobt@gmail.com>
2021-01-11 18:42:47 +0900

    힐러 텍스트도 변하게 만듬

--- Assets/Scripts/NpcPanel_Healer.cs
+++ Assets/Scripts/NpcPanel_Healer.cs
@@ -5,23 +5,32 @@ using UnityEngine.UI;
 
 public class NpcPanel_Healer : MonoBehaviour
 {
+    [HideInInspector]
+    public Text healerText;
+
     Player player = GameState.Instance.player;
+
     public void onClickHealButton() {
         int healAmount = player.GetStat().maxHp - player.hp;
 
         if(healAmount >= 1) {
             player.Heal(player.GetStat().maxHp);
-            //TODO : healAmount로 재화 채감
-            Debug.Log("힐됨!");
+            healerText.text = $"{healAmount}만큼 치료되었습니다. 나가세요.";
+            //TODO : 재화 차감
         } else {
-            Debug.Log("이미 풀피!");
+            healerText.text = "이미 최대 체력입니다.";
         }
         
+    }
+
+    void Awake() 
+    {
+        healerText = gameObject.transform.Find("HealerText").GetComponent<Text>();
     }
     // Start is called before the first frame update
     void Start()
     {
-        
+        healerText.text = "안녕하세요. 힐러입니다.";
     }
 
     // Update is called once per frame
