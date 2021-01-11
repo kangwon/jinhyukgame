a3705097124656af01bd5d955d0b8f21650b6372
quoobit <quoobt@gmail.com>
2021-01-11 18:32:16 +0900

    NPC-힐러 만듬

---
+++ Assets/Scripts/NpcPanel_Healer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NpcPanel_Healer : MonoBehaviour
+{
+    Player player = GameState.Instance.player;
+    public void onClickHealButton() {
+        int healAmount = player.GetStat().maxHp - player.hp;
+
+        if(healAmount >= 1) {
+            player.Heal(player.GetStat().maxHp);
+            //TODO : healAmount로 재화 채감
+            Debug.Log("힐됨!");
+        } else {
+            Debug.Log("이미 풀피!");
+        }
+        
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
