85aa0b7a48bd9138d28ba36084af2216fc96b89c
lee sangyeon <mpdark1227@gmail.com>
2021-01-12 19:44:15 +0900

    패널수정및 긍정,부정,중립이벤트가 1/3씩 나오도록 설정

--- Assets/Scripts/RandomPanelController.cs
+++ Assets/Scripts/RandomPanelController.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RandomPanelController : MonoBehaviour
 {
-    public StageChoice stageChoice;
+    StageChoice stageChoice;
+    Text randomDescription;
+    Text randomType;
     public void OnClickRandomButton()
     {
         stageChoice.MoveToNextStage();
@@ -12,9 +15,16 @@ public class RandomPanelController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        randomDescription = GameObject.Find("RandomPanel/RandomDescription").GetComponent<Text>();
+        randomType = GameObject.Find("RandomPanel/RandomType").GetComponent<Text>();
+        stageChoice = GameObject.Find("Canvas").GetComponent<StageChoice>(); 
     }
 
+    private void OnEnable()
+    {
+        randomType.text = $"";
+        randomDescription.text = $"";
+    }
     // Update is called once per frame
     void Update()
     {
