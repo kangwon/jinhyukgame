9391828d9eff50ab1a28ee8e22a88fe2bfda07a6
kangwon <kang_won@naver.com>
2021-01-14 21:27:51 +0900

    Add GameOverPanel & WorldClearPanel

---
+++ Assets/Scripts/WorldClearController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class WorldClearController : MonoBehaviour
+{
+    void Start()
+    {
+        var menuButton = GameObject.Find("Canvas").transform
+            .Find("WorldClearPanel/MenuButton").gameObject.GetComponent<Button>();
+        menuButton.onClick.AddListener(() => {
+            this.OnClickMenuButton();
+        });
+    }
+
+    void OnClickMenuButton()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
+}
