4c198460b651978eb45cd5e71db126adcc9c7048
sunder97 <sunder97@naver.com>
2021-01-17 15:15:32 +0900

    Enchant_Equip Make

--- Assets/Scripts/EnchantControl.cs
+++ Assets/Scripts/EnchantControl.cs
@@ -15,12 +15,11 @@ public class EnchantControl : MonoBehaviour
     List<int> enchantWeaponMny = new List<int>(new int[] { 25, 100, 250, 500, 750, 1000, });
     List<int> enchantEquipmentMny = new List<int>(new int[] { 50, 175, 400, 750, 1000, 1500, });
     List<string> prefixlist = new List<string>(new string[] { "broken", "weak", "normal", "strong", "amazing" });
+    List<string> typelist = new List<string>(new string[] { "weapon", "helmet", "armor", "shoes" });
     int minus_mny;
 
     List<Weapon> weaponList;
-    Helmet pHelmet;
-    Armor pArmor;
-    Shoes pShoes;
+    Equipment pHelmet, pArmor, pShoes;
     GameObject[] I_card = new GameObject[10];
     GameObject helcard;
     GameObject armcard;
@@ -36,6 +35,8 @@ public class EnchantControl : MonoBehaviour
 
     string beforeid;
     string checkprefix;
+    string checktype;
+    string checkrank;
     int changeprefix;
 
     void PrintCard()
@@ -55,18 +56,17 @@ public class EnchantControl : MonoBehaviour
         afterCard.transform.GetChild(0).GetComponent<Text>().text = $"선택된 카드 없음";
     }
     void CoinButton()
-    {
-        if (card_Index != -1)
+    {  
+        if (checkprefix == "4")
         {
-            if (checkprefix == "4")
-            {
-                Debug.Log("최대 강화 수식어입니다.");
-            }
-            else
-            {
-                Player player = GameState.Instance.player;
+            Debug.Log("최대 강화 수식어입니다.");
+        }
+        else
+        {
+            Player player = GameState.Instance.player;
 
-                Debug.Log($"index {card_Index}");
+            if (checktype == "weapon")
+            {
                 if (changeprefix / 10 == 0)
                     weaponList[card_Index] = JsonDB.GetWeapon($"weapon_00{changeprefix}");
                 else if (changeprefix / 100 == 0)
@@ -74,21 +74,40 @@ public class EnchantControl : MonoBehaviour
                 else
                     weaponList[card_Index] = JsonDB.GetWeapon($"weapon_{changeprefix}");
                 player.SetWeaponList(weaponList);
-
-                // 장비쪽 체크
-
-                player.SetEquipment(pHelmet, pArmor, pShoes);
-
-                Debug.Log($"count {weaponList.Count}");
-
-
-                // update 반복 방지 및 스테이지 넘김
-                EnchantOn = true;
-                stageChoice.MoveToNextStage();
             }
+            else if (checktype == "helmet")
+            {
+                if (changeprefix / 10 == 0)
+                    pHelmet = JsonDB.GetEquipment($"helmet{changeprefix / 100}_0{changeprefix % 100}");
+                else
+                    pHelmet = JsonDB.GetEquipment($"helmet{changeprefix / 100}_{changeprefix % 100}");
+
+                player.SetEquipment(pHelmet);
+            }
+            else if (checktype == "armor")
+            {
+                if (changeprefix / 10 == 0)
+                    pArmor = JsonDB.GetEquipment($"armor{changeprefix / 100}_0{changeprefix % 100}");
+                else
+                    pArmor = JsonDB.GetEquipment($"armor{changeprefix / 100}_{changeprefix % 100}");
+                player.SetEquipment(pArmor);
+            }
+            else if (checktype == "shoes")
+            {
+                if (changeprefix / 10 == 0)
+                    pShoes = JsonDB.GetEquipment($"shoes{changeprefix / 100}_0{changeprefix % 100}");
+                else
+                    pShoes = JsonDB.GetEquipment($"shoes{changeprefix / 100}_{changeprefix % 100}");
+                player.SetEquipment(pShoes);
+            }
+            // 장비쪽 체크
+
+
+
+            // update 반복 방지 및 스테이지 넘김
+            EnchantOn = true;
+            stageChoice.MoveToNextStage();
         }
-        else
-            Debug.Log("미선택");
     }
 
     void OnClickCard(int c_index)
@@ -102,41 +121,42 @@ public class EnchantControl : MonoBehaviour
 
             beforeid = $"{weaponList.ElementAt(card_Index).id}";
             checkprefix = beforeid.Substring(9);
+            checktype = "weapon";
             changeprefix = Convert.ToInt32(beforeid.Substring(7)) + 1;
 
             afterCard.transform.GetChild(0).GetComponent<Text>().text = prefixlist[changeprefix%10];
 
             if ($"{weaponList.ElementAt(c_index).rank}" == "common")
             {
-                enchantButton.transform.GetChild(0).GetComponent<Text>().text = $"{enchantEquipmentMny[0]} Coin  확인";
-                minus_mny = enchantEquipmentMny[0];
+                enchantButton.transform.GetChild(0).GetComponent<Text>().text = $"{enchantWeaponMny[0]} Coin  확인";
+                minus_mny = enchantWeaponMny[0];
             }
             else if ($"{weaponList.ElementAt(c_index).rank}" == "uncommon")
             {
-                enchantButton.transform.GetChild(0).GetComponent<Text>().text = $"{enchantEquipmentMny[1]} Coin  확인";
-                minus_mny = enchantEquipmentMny[1];
+                enchantButton.transform.GetChild(0).GetComponent<Text>().text = $"{enchantWeaponMny[1]} Coin  확인";
+                minus_mny = enchantWeaponMny[1];
             }
             else if ($"{weaponList.ElementAt(c_index).rank}" == "rare")
             {
-                enchantButton.transform.GetChild(0).GetComponent<Text>().text = $"{enchantEquipmentMny[2]} Coin  확인";
-                minus_mny = enchantEquipmentMny[2];
+                enchantButton.transform.GetChild(0).GetComponent<Text>().text = $"{enchantWeaponMny[2]} Coin  확인";
+                minus_mny = enchantWeaponMny[2];
             }
             else if ($"{weaponList.ElementAt(c_index).rank}" == "unique")
             {
-                enchantButton.transform.GetChild(0).GetComponent<Text>().text = $"{enchantEquipmentMny[3]} Coin  확인";
-                minus_mny = enchantEquipmentMny[3];
+                enchantButton.transform.GetChild(0).GetComponent<Text>().text = $"{enchantWeaponMny[3]} Coin  확인";
+                minus_mny = enchantWeaponMny[3];
             }
             else
             {
                 if ($"{weaponList.ElementAt(c_index).prefix}" != "strong")
                 {
-                    enchantButton.transform.GetChild(0).GetComponent<Text>().text = $"{enchantEquipmentMny[4]} Coin  확인";
-                    minus_mny = enchantEquipmentMny[4];
+                    enchantButton.transform.GetChild(0).GetComponent<Text>().text = $"{enchantWeaponMny[4]} Coin  확인";
+                    minus_mny = enchantWeaponMny[4];
                 }
                 else
                 {
-                    enchantButton.transform.GetChild(0).GetComponent<Text>().text = $"{enchantEquipmentMny[5]} Coin  확인";
-                    minus_mny = enchantEquipmentMny[5];
+                    enchantButton.transform.GetChild(0).GetComponent<Text>().text = $"{enchantWeaponMny[5]} Coin  확인";
+                    minus_mny = enchantWeaponMny[5];
                 }
             }
 
@@ -149,9 +169,79 @@ public class EnchantControl : MonoBehaviour
         }
     }
 
-    void OnClickCard2()
+    void OnClickCard2(string equipType)
     {
-        card_Index = -1;
+        // Debug.Log(equipType);
+
+        checktype = equipType;
+
+
+        if (equipType == "helmet" && pHelmet.id != "helmet")
+        {
+            beforeCard.transform.GetChild(0).GetComponent<Text>().text = $"{pHelmet.prefix}";
+            checkrank = $"{pHelmet.rank}";
+            checkprefix = $"{pHelmet.prefix}";
+            beforeid = $"{pHelmet.id}";
+        }
+        else if (equipType == "armor" && pArmor.id != "armor")
+        {
+            beforeCard.transform.GetChild(0).GetComponent<Text>().text = $"{pArmor.prefix}";
+            checkrank = $"{pArmor.rank}";
+            checkprefix = $"{pArmor.prefix}";
+            beforeid = $"{pArmor.id}";
+        }
+        else if (equipType == "shoes" && pShoes.id != "shoes")
+        {
+            beforeCard.transform.GetChild(0).GetComponent<Text>().text = $"{pShoes.prefix}";
+            checkrank = $"{pShoes.rank}";
+            checkprefix = $"{pShoes.prefix}";
+            beforeid = $"{pShoes.id}";
+        }
+        else
+        {
+            Debug.Log("아직 착용한 게 없어요");
+            return;
+        }
+
+        if (checkrank == "common")
+        {
+            enchantButton.transform.GetChild(0).GetComponent<Text>().text = $"{enchantEquipmentMny[0]} Coin  확인";
+            minus_mny = enchantEquipmentMny[0];
+        }
+        else if (checkrank == "uncommon")
+        {
+            enchantButton.transform.GetChild(0).GetComponent<Text>().text = $"{enchantEquipmentMny[1]} Coin  확인";
+            minus_mny = enchantEquipmentMny[1];
+        }
+        else if (checkrank == "rare")
+        {
+            enchantButton.transform.GetChild(0).GetComponent<Text>().text = $"{enchantEquipmentMny[2]} Coin  확인";
+            minus_mny = enchantEquipmentMny[2];
+        }
+        else if (checkrank == "unique")
+        {
+            enchantButton.transform.GetChild(0).GetComponent<Text>().text = $"{enchantEquipmentMny[3]} Coin  확인";
+            minus_mny = enchantEquipmentMny[3];
+        }
+        else
+        {
+            if (checkprefix != "strong")
+            {
+                enchantButton.transform.GetChild(0).GetComponent<Text>().text = $"{enchantEquipmentMny[4]} Coin  확인";
+                minus_mny = enchantEquipmentMny[4];
+            }
+            else
+            {
+                enchantButton.transform.GetChild(0).GetComponent<Text>().text = $"{enchantEquipmentMny[5]} Coin  확인";
+                minus_mny = enchantEquipmentMny[5];
+            }
+        }
+
+        // Debug.Log($"{beforeid}");
+
+        changeprefix = Convert.ToInt32(beforeid.Substring(6, 1))*100 + Convert.ToInt32(beforeid.Substring(8)) + 1;
+        afterCard.transform.GetChild(0).GetComponent<Text>().text = prefixlist[changeprefix % 10];
+        // Debug.Log($"{changeprefix}");
     }
 
     void Start()
@@ -180,10 +270,18 @@ public class EnchantControl : MonoBehaviour
         helcard = GameObject.Find("WeaponSelect/Helmet");
         helcard.GetComponent<Button>().onClick.AddListener(() =>
         {
-            OnClickCard2();
+            OnClickCard2("helmet");
         });
         armcard = GameObject.Find("WeaponSelect/Armor");
+        armcard.GetComponent<Button>().onClick.AddListener(() =>
+        {
+            OnClickCard2("armor");
+        });
         shocard = GameObject.Find("WeaponSelect/Shoes");
+        shocard.GetComponent<Button>().onClick.AddListener(() =>
+        {
+            OnClickCard2("shoes");
+        });
     }
 
     // Update is called once per frame
