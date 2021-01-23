deb010903a461fd4cdfd35f339e802be230b1211
lee sangyeon <mpdark1227@gmail.com>
2021-01-23 17:19:19 +0900

    UI와  장비, 아티펙트를  연동

--- Assets/Scripts/EquipPanelController.cs
+++ Assets/Scripts/EquipPanelController.cs
@@ -1,18 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using System.Linq;
 public class EquipPanelController : MonoBehaviour
 {
+    GameObject[] artifactIcon = new GameObject[3];
+    GameObject[] equipmentIcon = new GameObject[3];
+    bool firstStart = true;
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 0; i < 3; i++)
+        {
+            artifactIcon[i] = GameObject.Find($"EquipPanel/AT{i+1}");
+            equipmentIcon[i] = GameObject.Find($"EquipPanel/JB{i+1}");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    private void OnEnable()
+    {
+        if (firstStart) firstStart = false;
+        else
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                if (i < GameState.Instance.player.GetArtifacts().Count)
+                    artifactIcon[i].transform.GetChild(0).GetComponent<Text>().text = GameState.Instance.player.GetArtifacts()?.ElementAt(i).name;
+                else
+                    artifactIcon[i].transform.GetChild(0).GetComponent<Text>().text = "";
+            }
+            equipmentIcon[0].transform.GetChild(0).GetComponent<Text>().text = GameState.Instance.player.GetHelmet()?.name;
+            equipmentIcon[1].transform.GetChild(0).GetComponent<Text>().text = GameState.Instance.player.GetArmor()?.name;
+            equipmentIcon[2].transform.GetChild(0).GetComponent<Text>().text = GameState.Instance.player.GetShoes()?.name;
+        }
     }
 }
