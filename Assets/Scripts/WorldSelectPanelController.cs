7f297efe416817def6317a971a8f89b555892d6a
kangwon <kang_won@naver.com>
2021-02-01 21:24:04 +0900

    Add WorldSelectPanel

---
+++ Assets/Scripts/WorldSelectPanelController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WorldSelectPanelController : MonoBehaviour
+{
+    public delegate void WorldSelectCallback(WorldId selectedWorld);
+
+    Button WorldButton1, WorldButton2;
+
+    void Start()
+    {
+        WorldButton1 = GameObject.Find("WorldSelectPanel/WorldButton1").GetComponent<Button>();
+        WorldButton2 = GameObject.Find("WorldSelectPanel/WorldButton2").GetComponent<Button>();
+
+        this.gameObject.SetActive(false);
+    }
+
+    public void DisplayPanel(WorldId world1, WorldId world2, WorldSelectCallback callback)
+    {
+        WorldButton1.onClick.AddListener(() => callback(world1));
+        WorldButton2.onClick.AddListener(() => callback(world2));
+
+        this.transform.localPosition = StageChoice.PanelDisplayPosition;
+        this.gameObject.SetActive(true);
+    }
+}
