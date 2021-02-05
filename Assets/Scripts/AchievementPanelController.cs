2332ce89b4897592366f150eebb6f749bdc837bf
kangwon <kang_won@naver.com>
2021-02-05 23:47:28 +0900

    Disappear AchievementPanelController after 3 sec

--- Assets/Scripts/AchievementPanelController.cs
+++ Assets/Scripts/AchievementPanelController.cs
@@ -6,6 +6,8 @@ using UnityEngine.UI;
 
 public class AchievementPanelController : MonoBehaviour
 {
+    const float INTERVAL = 3f;
+
     Text Title;
     Text Description;
 
@@ -27,5 +29,13 @@ public class AchievementPanelController : MonoBehaviour
         this.gameObject.SetActive(true);
         Title.text = achievement.name;
         Description.text = achievement.description;
+
+        StartCoroutine(Disappear());
     }
+
+    IEnumerator Disappear()
+     {
+        yield return new WaitForSeconds(INTERVAL);
+        this.gameObject.SetActive(false);
+     }
 }
